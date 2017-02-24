535f5c2b4cf90429cb682e66dfad99411acecf82
stajama <stajama@yahoo.com>
2017-02-23 20:26:54 -0600

    Fixed copy/paste fuck up

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,5 @@
 
+
 import random
 library = ['acres',
 		   'adult',
@@ -127,34 +128,6 @@ library = ['acres',
 		   'wolf',
 		   'zoo']
 
-def displayer(word,guesslist,strikes):
-	printlist = (' _'*len(word))+'  '
-	for letter in range(len(word)):
-		if word[letter] in guesslist:
-			printlist = printlist[:(letter*2+1)]+word[letter]+printlist[(letter*2+2):]
-	print(printlist)
-	print(['',' =',' ==',' ==\n l',' ==\n l\n l',' ==\n l\n l\n 0',' ==\n l\n l\n 0\n\\',' ==\n l\n l\n 0\n\\!',' ==\n l\n l\n 0\n\\!/',' ==\n l\n l\n 0\n\\!/\n i',' ==\n l\n l\n 0\n\\!/\n i\n /',' ==\n l\n l\n 0\n\\!/\n i\n /\\'][strikes])
-	return
-
-def setup():
-	name = input("Enter your name")
-	score = 0
-	return name,score
-	
-def lettercheck(word,letter):
-	if letter in word:
-		return True
-	else:
-		return False
-		
-def endgame(word,guesslist):
-	for letter in word:
-		if not letter in guesslist:
-			return False
-	return True
-	
-import random
-library = ['acres','adult','advice','arrangement','attempt','august','autumn','border','breeze','brick','calm','canal','casey','cast','chose','claws','coach','constantly','contrast','cookies','customs','damage','danny','deeply','depth','discussion','doll','donkey','egypt','ellen','essential','exchange','exist','explanation','facing','film','finest','fireplace','floating','folks','fort','garage','grabbed','grandmother','habit','happily','harry','heading','hunter','illinois','image','independent','instant','january','kids','label','lee','lungs','manufacturing','martin','mathematics','melted','memory','mill','mission','monkey','mount','mysterious','neighborhood','norway','nuts','occasionally','official','ourselves','palace','pennsylvania','philadelphia','plates','poetry','policeman','positive','possibly','practical','pride','promised','recall','relationship','remarkable','require','rhyme','rocky','rubbed','rush','sale','satellites','satisfied','scared','selection','shake','shaking','shallow','shout','silly','simplest','slight','slip','slope','soap','solar','species','spin','stiff','swung','tales','thumb','tobacco','toy','trap','treated','tune','university','vapor','vessels','wealth','wolf','zoo']
 
 def displayer(word,guesslist,strikes):
 	printlist = (' _'*len(word))+'  '
@@ -222,5 +195,4 @@ def hangman():
 	
 if __name__ == "__main__":
   hangman()   	
-	
 	
